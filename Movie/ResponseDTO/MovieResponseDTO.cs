d79e340a64c3251e0c47e2069dc53d2baad178b3
WwangVinh <wwangvinh04@gmail.com>
2025-04-17 08:50:20 +0700

    Add files via upload

--- Movie/ResponseDTO/MovieResponseDTO.cs
+++ Movie/ResponseDTO/MovieResponseDTO.cs
@@ -3,7 +3,7 @@
     public class MovieResponseDTO
     {
         public string Title { get; set; } = null!;
-        public string? LinkFilmUrl { get; set; }
+        public string? LinkFilmUrl { get; set; } 
         public int YearReleased { get; set; }
         //public string National { get; set; }
         public string Categories { get; set; }
